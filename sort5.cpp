a5a73a7105a07bdb5ee8555c173a7bbc056d4812
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-03-14 22:00:31 +0300

    Create sort5.cpp

---
+++ sort5.cpp
@@ -0,0 +1,43 @@
+sort27.cpp File Reference
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <clocale>
+Go to the source code of this file.
+
+Functions
+void 	quick_sort (vector< int > &v, int begin0, int end0)
+ 	������� ������� ���������� More...
+ 
+int 	main ()
+ 
+Function Documentation
+
+int main	(		)	
+������� ������.
+
+��������� ��� ������� �� 0 �� 19.
+
+������������ ��� ������
+
+������� ������� ����������
+
+Definition at line 41 of file sort27.cpp.
+
+void quick_sort	(	vector< int > & 	v,
+int 	begin0,
+int 	end0 
+)		
+������� ������� ����������
+
+�����, ������������ ������� ����� ����������
+
+��������� �� ������
+
+��������� �� �����
+
+������ �������
+
+Definition at line 16 of file sort27.cpp.
+
