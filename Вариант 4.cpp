b007ef3b83613cfbc59a5fad038a60bc8b557069
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-03-18 15:15:56 +0300

    Create Вариант 4.cpp

---
+++ Вариант 4.cpp
@@ -0,0 +1,36 @@
+// Шестнадцатиричная система счисления
+#include "stdafx.h"
+#include <stdlib.h>
+#include <iostream>
+#include <locale.h>
+#include <conio.h>
+using namespace std;
+
+#include <iostream>
+
+using namespace std;
+
+class Test
+{
+private:
+	int x;
+public:
+	
+	Test(){}
+	Test(int a){ x = a; }
+	~Test(){}
+	void Print(){ cout << "Число: " << x << ". Число в 16-й СС: " << hex << x << dec << endl; }
+	void Print(int a){ cout << "Число: " << a << ". Число в 16-й СС: " << hex << a << endl; }
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	Test t(110);
+	t.Print();
+	system("pause");
+	return 0;
+}
+
