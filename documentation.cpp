852b236b080c6e780a52de342f383147f0c6b6ed
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-04-04 17:05:33 +0300

    Create documentation.cpp

---
+++ documentation.cpp
@@ -0,0 +1,75 @@
+List< T > Class Template Reference
+Public Member Functions
+void 	push_front (const T &val)
+ 
+void 	push_back (const T &val)
+ 
+T 	pop_front ()
+ 
+T 	pop_back ()
+ 
+bool 	empty () const
+ 
+virtual void 	clear ()
+ 
+ 	List ()
+ 
+ 	List (const List &l)
+ 
+virtual 	~List ()
+ 
+Detailed Description
+
+template<typename T>
+class List< T >
+
+Definition at line 9 of file ConsoleApplication7.cpp.
+
+Constructor & Destructor Documentation
+
+template<typename T>
+List< T >::List	(		)	
+inline
+Definition at line 124 of file ConsoleApplication7.cpp.
+
+template<typename T>
+List< T >::List	(	const List< T > & 	l	)	
+inline
+Definition at line 130 of file ConsoleApplication7.cpp.
+
+template<typename T>
+virtual List< T >::~List	(		)	
+inlinevirtual
+Definition at line 145 of file ConsoleApplication7.cpp.
+
+Member Function Documentation
+
+template<typename T>
+virtual void List< T >::clear	(		)	
+inlinevirtual
+Definition at line 109 of file ConsoleApplication7.cpp.
+
+template<typename T>
+bool List< T >::empty	(		)	const
+inline
+Definition at line 103 of file ConsoleApplication7.cpp.
+
+template<typename T>
+T List< T >::pop_back	(		)	
+inline
+Definition at line 76 of file ConsoleApplication7.cpp.
+
+template<typename T>
+T List< T >::pop_front	(		)	
+inline
+Definition at line 65 of file ConsoleApplication7.cpp.
+
+template<typename T>
+void List< T >::push_back	(	const T & 	val	)	
+inline
+Definition at line 45 of file ConsoleApplication7.cpp.
+
+template<typename T>
+void List< T >::push_front	(	const T & 	val	)	
+inline
+Definition at line 30 of file ConsoleApplication7.cpp.
