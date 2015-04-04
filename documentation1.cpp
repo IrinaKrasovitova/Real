15b29e75a8c654b37c4937cfa9611bf10dbf0130
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-04-04 17:36:09 +0300

    Create documentation1.cpp

---
+++ documentation1.cpp
@@ -0,0 +1,80 @@
+BinarySearchTree< T > Class Template Reference
+Public Member Functions
+ 	BinarySearchTree ()
+ 
+bool 	isEmpty () const
+ 
+void 	print_inorder ()
+ 
+void 	inorder (tree_node *)
+ 
+void 	print_preorder ()
+ 
+void 	preorder (tree_node *)
+ 
+void 	print_postorder ()
+ 
+void 	postorder (tree_node *)
+ 
+void 	insert (T)
+ 
+void 	remove (T)
+ 
+bool 	search (T)
+ 
+Detailed Description
+
+template<class T>
+class BinarySearchTree< T >
+
+Definition at line 6 of file ConsoleApplication7.cpp.
+
+Constructor & Destructor Documentation
+
+template<class T>
+BinarySearchTree< T >::BinarySearchTree	(		)	
+inline
+Definition at line 17 of file ConsoleApplication7.cpp.
+
+Member Function Documentation
+
+template<class T >
+void BinarySearchTree< T >::inorder	(	tree_node * 	p	)	
+Definition at line 238 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::insert	(	T 	d	)	
+Definition at line 35 of file ConsoleApplication7.cpp.
+
+template<class T>
+bool BinarySearchTree< T >::isEmpty	(		)	const
+inline
+Definition at line 21 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::postorder	(	tree_node * 	p	)	
+Definition at line 271 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::preorder	(	tree_node * 	p	)	
+Definition at line 254 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::print_inorder	(		)	
+Definition at line 233 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::print_postorder	(		)	
+Definition at line 265 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::print_preorder	(		)	
+Definition at line 249 of file ConsoleApplication7.cpp.
+
+template<class T >
+void BinarySearchTree< T >::remove	(	T 	d	)	
+Definition at line 105 of file ConsoleApplication7.cpp.
+
+template<class T >
+bool BinarySearchTree< T >::search	(	T 	d	)	
+Definition at line 65 of file ConsoleApplication7.cpp.
