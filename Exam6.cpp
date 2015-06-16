24ce03f43dc12ddff594b3731b1f056f7efccf4d
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-06-16 16:04:05 +0300

    Create Exam6.cpp

---
+++ Exam6.cpp
@@ -0,0 +1,49 @@
+// Exam1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class integer
+{
+	int a;
+public:
+	integer(int a)
+	{
+		this->a = a;
+	}
+
+	void print()
+
+	{
+		cout << "\na=" << a;
+		if (a % 2 == 0)
+			cout << "\nChetnoe";
+		else cout << "\nNechetnoe";
+
+	}
+};
+
+
+int main(int argc, char* argv[])
+{
+
+
+
+	/*printf("%d ", 9);
+	if (9 % 2 == 0)
+		cout << "\nChetnoe";
+	else cout << "\nNechetnoe";*/
+
+	integer p1(9);
+	integer *pp;
+	pp = &p1;
+	//p1.print();
+	pp->print();
+	
+	system("pause");
+	return 0;
+
+}
+
