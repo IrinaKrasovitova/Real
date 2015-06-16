a9b29713cc86f1d491a6770ad63ebd3ab5ec371c
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-06-16 16:25:33 +0300

    Update Exam10.cpp

--- Exam10.cpp
+++ Exam10.cpp
@@ -1,1 +1,54 @@
 
+#include "stdafx.h" 
+#include <iostream> 
+ using namespace std; 
+ 
+ 
+ 
+ 
+ class square //  класс - квадрат 
+ { 
+ private: 
+ 	double a; 
+ 	 
+ public: 
+ 	square(double a) // Конструктор для инициализации полей 
+ 	{ 
+ 		this->a = a; 
+ 		 
+ 	} 
+ 
+ 
+ 	void print() // Метод для печати полей 
+ 	{ 
+ 		cout << "\na=" << a; // Печатаем значения полей 
+ 		cout << "\ns=" << 4*a; 
+ 	} 
+ }; 
+ 
+ 
+ 
+ 
+ int main(int argc, char* argv[]) 
+ { 
+ 	square p1(3); // Создается объект с вызовом конструктора 
+ 	 
+ 	square *pp; // Указатель типа базового класса 
+ 	pp = &p1; // Настраиваем на объект базового класса 
+ 	pp->print(); // Вызов метода через указатель 
+ 	 
+ 	pp->print(); // Вызов метода через указатель, вызывается метод класса square 
+ 	 
+ 	 
+ 
+ 
+ 	system("pause"); 
+ 	return 0; 
+ } 
+ 
+
+
+ 
+   
+
+
